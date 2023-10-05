5d81f055d251b515138b4acebe2313e360182c47
mugunthan <mugudhoni2001@gmail.com>
2023-10-05 12:50:53 +0530

    first commit

---
+++ to-do/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import TodoWrapper  from "./Components/TodoWrapper";
+
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper name="mani" />
+    </div>
+  );
+}
+
+export default App;
